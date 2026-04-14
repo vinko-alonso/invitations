92871c4f3d7fd45249cbc32a5cea81338d45000d
Alonso Zamorano <alonso.zamorano@uc.cl>
2026-04-13 22:52:36 -0400

    vite congif

--- invitations/vite.config.js
+++ invitations/vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    transformer: 'postcss',
-  },
   build: {
-    cssMinify: 'esbuild',
+    cssMinify: false,
   },
 })
